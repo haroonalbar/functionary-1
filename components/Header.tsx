47691371653df050d8ea41e6b6af9506149ad789
haroonalbar <haroonalbarx@gmail.com>
2022-12-27 13:03:42 +0530

    part2(home) done

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,18 +5,18 @@ type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <div className=' z-30 sticky'>
+    <div className=' z-30 sticky top-0'>
 
     <div className=' py-5 flex justify-between space-x-5 items-center'>
         {/* <img src='./public/image/image1.png' alt="logo" className='w-10 h-10 object-contain' /> */}
-        <Image src={logo} className='w-[130px]'/>
+        <Image src={logo} alt='logo' className='w-[130px]'/>
         <div className='flex space-x-28 '>
-          <div className=' uppercase text-[16px] text-[#808080] hover:text-black py-2  border-b-4 border-[rgba(0,0,0,0)] hover:border-[#95C24D]  '>Home</div>
-          <div className=' uppercase text-[16px] text-[#808080] hover:text-black py-2  border-b-4 border-[rgba(0,0,0,0)] hover:border-[#95C24D] '>Services</div>
-          <div className=' uppercase text-[16px] text-[#808080] hover:text-black py-2  border-b-4 border-[rgba(0,0,0,0)] hover:border-[#95C24D] '>Utilities</div>
-          <div className=' uppercase text-[16px] text-[#808080] hover:text-black py-2  border-b-4 border-[rgba(0,0,0,0)] hover:border-[#95C24D] '>About us</div>
-          <div className=' uppercase text-[16px] text-[#808080] hover:text-black py-2  border-b-4 border-[rgba(0,0,0,0)] hover:border-[#95C24D] '>Vision</div>
-          <div className=' uppercase text-[16px] text-[#808080] hover:text-black py-2  border-b-4 border-[rgba(0,0,0,0)] hover:border-[#95C24D] '>Contact us</div>
+          <div className=' header '>Home</div>
+          <div className=' header '>Services</div>
+          <div className=' header '>Utilities</div>
+          <div className=' header '>About us</div>
+          <div className=' header '>Vision</div>
+          <div className=' header '>Contact us</div>
         </div>
     </div>
     </div>
--- components/Part1.tsx
+++ components/Part1.tsx
@@ -1,11 +1,26 @@
 import Image from 'next/image'
 import React from 'react'
+
+// images 
 import man from '../public/image/image2.png'
+import logo1 from '../public/part2/logo1.svg'
+import logo2 from '../public/part2/logo2.svg'
+import logo3 from '../public/part2/logo3.svg'
+import logo4 from '../public/part2/logo4.svg'
+import logo5 from '../public/part2/logo5.svg'
+import logo6 from '../public/part2/logo6.svg'
+import logo7 from '../public/part2/logo7.svg'
+import logo8 from '../public/part2/logo8.svg'
+import logo9 from '../public/part2/logo9.svg'
+import logo10 from '../public/part2/logo10.svg'
+
+
+
 type Props = {}
 
 const Part1 = (props: Props) => {
   return (
-    <div className=''>
+    <div>
      <div className=' bg-[#FAF8F8] flex justify-between items-center  mx-auto'>
         <div className='ml-[140px] mx-auto'>
         <div className='flex space-x-1 mt-[151px]'>
@@ -18,20 +33,28 @@ const Part1 = (props: Props) => {
             See all products</button>    
         </div>
         {/* <img src="public/image/image2.png" alt="man" className=''/> */}
-        <Image src={man} className='w-[948px] z-0 clip1' />
+        <Image src={man} alt='man' className='w-[948px] z-0 clip1' />
 
      </div>
      {/* part2 */}
-     <div className='flex space-x-32 justify-between px-20'>
-       <p className='max-w-[300px]'> We support wide range of industries with our services and products</p>
-       <div className='grid grid-cols-3 space-x-2'>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
+     <div className='flex justify-between'>
+       <p className='max-w-[500px] text-[36px] ml-[140px] mt-[118px] mb-[131px]'> We support wide range of industries with our services and products</p>
+       <div className='grid grid-cols-4 mt-[57px] mr-[140px] space-x-5'>
+        <Image alt='logo' src={logo1}/>
+        <div className='h-[50px] w-[69px] bg-[#95C24D]/10 '/>
+        <div></div>
+        <div></div>
+        <Image alt='logo' src={logo2}/>
+        <Image alt='logo' src={logo4}/>
+        <Image alt='logo' src={logo7}/> 
+        <Image alt='logo' src={logo10}/> 
+        <Image alt='logo' src={logo3}/>
+        <Image alt='logo' src={logo5}/>
+        <div className='h-[50px] w-[69px] bg-[#95C24D]/10 '/>
+        <Image alt='logo' src={logo9}/> 
+        <div></div>
+        <Image alt='logo' src={logo6}/>
+        <Image alt='logo' src={logo8}/> 
        </div>
      </div>
     </div>
